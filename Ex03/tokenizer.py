5a10464a981899b5135e0140f2fac6b01d67712b
Mohsen Mansouryar <mohsen.brian@gmail.com>
2015-05-28 18:13:10 +0200

    finished part b of Ex03

---
+++ Ex03/tokenizer.py
@@ -0,0 +1,34 @@
+from __future__ import print_function
+import codecs
+from pylab import plt
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+import sys
+
+# TODO: in the end, experiment with snowball stemmer and compare results!
+# en_stem = nltk.snowball.EnglishStemmer().stem
+en_stem = LancasterStemmer().stem
+
+# We're just using this stop word list from nltk, we could as well copy paste the list here
+stop_words = nltk.corpus.stopwords.words('english')
+
+# punctuation marks
+SPLITTING_CHAR_SET = (' ', ',', ".", "'", '!', '?', ';', ':', '"', ')', '(', '[', ']', '{', '}')
+
+bonus_part = False
+
+def tokenize(s, stem = True):
+	'''
+	s is a string to tokenize
+	stem enabled would apply stemming
+	'''
+	tokens, token = [], ''
+	for c in s.lower():
+		if c in SPLITTING_CHAR_SET:
+			if token and not token in stop_words:
+				if stem: token = en_stem(token)
+				tokens.append(token)
+			token = ''
+		else:
+			token += c
+	return tokens
--- Ex03/wsd.py
+++ Ex03/wsd.py
@@ -1,12 +1,19 @@
 from __future__ import division
 import os, codecs
+from tokenizer import tokenize
 
 DATA_DIR = './data'
 
+# words is a dictionary of all our words
+# each key corresponds to a word
+# each value is a list of tuples all in the form (sense, definition)
 words = dict()
+
+# contexts is the data we're working on
+# each element is a triple of the form (word, sense, context)
 contexts = []
 
-def process_data():
+def read_data():
 	global words, contexts
 
 	for filename in os.listdir(DATA_DIR):
@@ -21,10 +28,10 @@ def process_data():
 						if line.strip(): # going through definition for last sense
 							definition += line
 						else: # end of definition for last sense
-							words[word].append((sense, definition.strip()))
+							words[word].append([sense, definition.strip()])
 							definition = ''
 				if definition: # definition for last sense
-					words[word].append((sense, definition.strip()))
+					words[word].append([sense, definition.strip()])
 		elif filename.endswith('.test'): # test file
 			with codecs.open(os.path.join(DATA_DIR, filename), encoding='utf-8') as document:
 				context = ''
@@ -35,26 +42,33 @@ def process_data():
 						if line.strip(): # going through context for last word-sense pair
 							context += line
 						else: # end of last context
-							contexts.append((word, sense, context.strip()))
+							contexts.append([word, sense, context.strip()])
 							context = ''
 				if context: # context for last word-sense pair
-					contexts.append((word, sense, context.strip()))
+					contexts.append([word, sense, context.strip()])
 		else: # readme or some other file
 			continue
 
+def normalize_data():
+	for word in words:
+		# converting each sense-definition pair to sense-normalized_definition_tokens
+		words[word] = map(lambda pair: [pair[0], tokenize(pair[1])], words[word])
+
 def main():
-	print 'Processing data...'
-	process_data()
+	print 'Reading data...'
+	read_data()
+	print 'Normalizing data...'
+	normalize_data()
 
 	print 'Printing some results...'
 	
-	## Uncomment to print word data
-	# for word, senses in words.items():
-	# 	print '#' * 45
-	# 	print word, len(senses)
-	# 	for sense, definition in senses:
-	# 		print sense + ': ' + definition
-	# 		print
+	# Uncomment to print word data
+	for word, senses in words.items():
+		print '#' * 45
+		print word, len(senses)
+		for sense, definition in senses:
+			print sense + ': ' + str(definition)
+			print
 	
 	## Uncomment to print some word-sense-context triples
 	# print 	
