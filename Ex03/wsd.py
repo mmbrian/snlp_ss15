797ce34f9f9cf0756ff87657b11abdca5c2d053f
Mohsen Mansouryar <mohsen.brian@gmail.com>
2015-05-28 19:03:21 +0200

    finished algorithms, only plotting remains

--- Ex03/wsd.py
+++ Ex03/wsd.py
@@ -50,27 +50,58 @@ def read_data():
 			continue
 
 def normalize_data():
+	global contexts
 	for word in words:
 		# converting each sense-definition pair to sense-normalized_definition_tokens
 		words[word] = map(lambda pair: [pair[0], tokenize(pair[1])], words[word])
+	contexts = map(lambda triple: [triple[0], triple[1], tokenize(triple[2])], contexts)
+
+
+def overlap(a, b):
+	# converting lists to sets
+	a, b = set(a), set(b)
+	return 2 * len(a & b) / (len(a) + len(b))
+
+def wsd(context, senses):
+	'''
+	context is the context in which we want to do word sense disambiguation
+	senses is a list of sense-definition pairs
+	returns the sense with the largest overlap
+	'''
+	senses = map(lambda pair: [pair[0], overlap(pair[1], context)], senses)
+	return sorted(senses, key = lambda pair: pair[1], reverse = True)[0][0]
+
 
 def main():
 	print 'Reading data...'
 	read_data()
 	print 'Normalizing data...'
 	normalize_data()
+	print 'Processing...'
 
-	print 'Printing some results...'
+	# for each word, 1st element corresponds to the number of correct guesses
+	# 2nd element is the number of incorrect guesses
+	results = dict(zip(words.keys(), [[0, 0] for i in xrange(len(words))]))
+	for ctxt in contexts:
+		word, sense, context = ctxt
+		result = wsd(context, words[word])
+		correct = sense == result
+		results[word][0 if correct else 1] += 1
+	for k, v in results.items():
+		print k, v, v[0]/(sum(v)) * 100
+	print 'Total: ', sum([v[0]/(sum(v)) * 100 for v in results.values()])/len(results), len(results)
+	print sum(sum(v) for v in results.values()), 'Contexts'
+
+	# # print 'Printing some results...'
+	# # Uncomment to print word data
+	# for word, senses in words.items():
+	# 	print '#' * 45
+	# 	print word, len(senses)
+	# 	for sense, definition in senses:
+	# 		print sense + ': ' + str(definition)
+	# 		print
 	
-	# Uncomment to print word data
-	for word, senses in words.items():
-		print '#' * 45
-		print word, len(senses)
-		for sense, definition in senses:
-			print sense + ': ' + str(definition)
-			print
-	
-	## Uncomment to print some word-sense-context triples
+	# # Uncomment to print some word-sense-context triples
 	# print 	
 	# for i in xrange(10):
 	# 	print '#' * 45
